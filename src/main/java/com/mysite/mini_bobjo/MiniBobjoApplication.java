9a17bec45dc2486fb77bc8a7b09cd5de64020cb3
khj(YS) <pridom03@gmail.com>
2024-10-10 09:41:29 +0900

    초기설정

---
+++ src/main/java/com/mysite/mini_bobjo/MiniBobjoApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.mini_bobjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBobjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBobjoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/mini_bobjo/MiniBobjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.mini_bobjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBobjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
